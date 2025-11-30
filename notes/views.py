ba7cdcae0a3a8dd51f6fe9b2c57d5d7f40d92077
Soujanya jain <149358727+Soujana77@users.noreply.github.com>
2025-11-30 22:20:22 +0530

    feat: add PDF text extraction and show_text page

--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from .forms import UploadPDFForm
+from .utils import extract_text
 
 def home(request):
     return render(request, 'notes/home.html')
-from .forms import UploadPDFForm
 
 def upload_pdf(request):
     if request.method == 'POST':
         form = UploadPDFForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return render(request, 'notes/success.html')
+            saved_file = form.save()  # save file in media folder
+            pdf_path = saved_file.file.path  # full path
+            
+            # extract text
+            full_text = extract_text(pdf_path)
+
+            # send extracted text to page
+            return render(request, 'notes/show_text.html', {'text': full_text})
     else:
         form = UploadPDFForm()
+    
     return render(request, 'notes/upload.html', {'form': form})
