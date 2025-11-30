9f0e44715f79443d53b5ce734c823e3518a77245
Soujanya jain <149358727+Soujana77@users.noreply.github.com>
2025-11-30 22:08:35 +0530

    feat: add navigation link to upload page

--- notes/urls.py
+++ notes/urls.py
@@ -5,4 +5,5 @@ app_name = "notes"
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('upload/', views.upload_pdf, name='upload'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'notes/home.html')
+from .forms import UploadPDFForm
+
+def upload_pdf(request):
+    if request.method == 'POST':
+        form = UploadPDFForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'notes/success.html')
+    else:
+        form = UploadPDFForm()
+    return render(request, 'notes/upload.html', {'form': form})
